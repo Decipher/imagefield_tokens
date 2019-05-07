f22da35ba07766aaf7aef397e7e0aae6507ef6cd
Yaroslav Samoilenko <Yaroslav_Samoilenko@epam.com>
2019-05-07 18:24:15 +0300

    ImageFieldTokens D8 project merging started

--- README.txt
+++ README.txt
@@ -1,41 +1,27 @@
-INTRODUCTION
-------------
+Image Field Tokens D8
 
-The ImageField Tokens  module extends the default functionality
-of Image Widget and Formatter and adding the ability
-to specify default values and use content entity tokens in the
-Alt and Title text.
+The ImageField Tokens module extends the default functionality of ImageField
+module by adding the ability to use node tokens in the ALT and Title text.
 
-INSTALLATION
--------------
-
-1. Place the entire imagefield_tokens directory into your Drupal
-   modules directory.
-
-2. Enable the imagefield_tokens module by navigate to admin/modules
-
-REQUIREMENTS
--------------
-
-This module requires the following modules:
-
-- Image (included in Drupal 8 Core)
-- Token (https://www.drupal.org/project/token)
-
-
-CONFIGURATION
--------------
-
-The module has no menu or modifiable settings. There is no configuration.
-User can configure each "Image Field Tokens" widget at entity/node display form.
-
-
-MAINTAINERS
--------------
 ImageField Tokens 5.x & 6.x were written by Stuart Clark (deciphered).
 - http://stuar.tc/lark
 
 ImageField Tokens 7.x was written by eosrei - http://eosrei.net
 
-ImageField Tokens 8.x was written by ysamoylenko:
-https://www.drupal.org/u/ysamoylenko
+
+Features
+--------------------------
+
+* Configurable ALT and Title text now use node tokens instead of user tokens.
+
+
+Frequently Asked Questions
+--------------------------
+
+Q. Aren't tokens already supported in the ImageField module?
+
+A. Only User tokens are supported in the ImageField module.
+
+   Node tokens allow you to use the Node ID, Node Title, Node creation date and
+   much more in your ALT/Title text where User tokens are very limited.
+
---
+++ src/Plugin/Field/FieldFormatter/ImageFieldTokensFormatter.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Drupal\image_field_tokens\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Url;
+use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatter;
+
+/**
+ * Plugin implementation of the 'image' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "image_field_tokens",
+ *   label = @Translation("Image Field Tokens"),
+ *   field_types = {
+ *     "image"
+ *   },
+ *   quickedit = {
+ *     "editor" = "image"
+ *   }
+ * )
+ */
+class ImageFieldTokensFormatter extends ImageFormatter {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    $files = $this->getEntitiesToView($items, $langcode);
+
+    // Early opt-out if the field is empty.
+    if (empty($files)) {
+      return $elements;
+    }
+
+    $url = NULL;
+    $image_link_setting = $this->getSetting('image_link');
+    // Check if the formatter involves a link.
+    if ($image_link_setting === 'content') {
+      $entity = $items->getEntity();
+      if (!$entity->isNew()) {
+        $url = $entity->urlInfo();
+      }
+    }
+    elseif ($image_link_setting === 'file') {
+      $link_file = TRUE;
+    }
+
+    $image_style_setting = $this->getSetting('image_style');
+
+    // Collect cache tags to be added for each item in the field.
+    $base_cache_tags = [];
+    if (!empty($image_style_setting)) {
+      $image_style = $this->imageStyleStorage->load($image_style_setting);
+      $base_cache_tags = $image_style->getCacheTags();
+    }
+
+    foreach ($files as $delta => $file) {
+      $cache_contexts = [];
+      if (isset($link_file)) {
+        $image_uri = $file->getFileUri();
+        // @todo Wrap in file_url_transform_relative(). This is currently
+        // impossible. As a work-around, we currently add the 'url.site' cache
+        // context to ensure different file URLs are generated for different
+        // sites in a multisite setup, including HTTP and HTTPS versions of the
+        // same site. Fix in https://www.drupal.org/node/2646744.
+        $url = Url::fromUri(file_create_url($image_uri));
+        $cache_contexts[] = 'url.site';
+      }
+      $cache_tags = Cache::mergeTags($base_cache_tags, $file->getCacheTags());
+
+      // Extract field item attributes for the theme function, and unset them
+      // from the $item so that the field template does not re-render them.
+      $item = $file->_referringItem;
+      $item_attributes = $item->_attributes;
+      unset($item->_attributes);
+      // Get item values.
+      $item_values = $item->getValue();
+      // Get entity from request.
+      $request_params = \Drupal::routeMatch()->getParameters()->all();
+      $entity = array_pop($request_params);
+      $entity_type = $entity->getEntityTypeId();
+      // Replace entity tokens.
+      $alt_token = \Drupal::token()->replace($item_values['alt'], [$entity_type => $entity]);
+      $title_token = \Drupal::token()->replace($item_values['title'], [$entity_type => $entity]);
+      // Set converted values to the item.
+      $item_values['alt'] = $alt_token;
+      $item_values['title'] = $title_token;
+      $item->setValue($item_values);
+
+      $elements[$delta] = [
+        '#theme' => 'image_formatter',
+        '#item' => $item,
+        '#item_attributes' => $item_attributes,
+        '#image_style' => $image_style_setting,
+        '#url' => $url,
+        '#cache' => [
+          'tags' => $cache_tags,
+          'contexts' => $cache_contexts,
+        ],
+      ];
+    }
+
+    return $elements;
+  }
+
+}
